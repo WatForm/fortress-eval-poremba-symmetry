c573b5c93b68fda3083a53eb73bc7e7c5ca88f91
Nancy Day <nday@uwaterloo.ca>
2022-02-05 15:59:07 -0500

    removed goalcounter in run-tests.py

--- python/run-tests.py
+++ python/run-tests.py
@@ -30,7 +30,6 @@ inputfilelist = thisdirprefix + "results/2022-01-15-"+goal+"-file-scope-list.txt
 
 filecounter = 1  # start at first line of files; counting starts at 1
                  # change above if process does not finish and we have to restart
-goalcounter = 0 # need to reset this if change filecounter!!!
 
 now = datetime.now()
 dt_string = now.strftime("%Y-%m-%d-%H-%M-%S")
