a3ff188428145938f419e0a045dba024cdd867f1
Nancy Day <nday@uwaterloo.ca>
2022-03-11 11:24:08 -0500

    Update bisection-multi-fortress-versions.py

--- python/bisection-multi-fortress-versions.py
+++ python/bisection-multi-fortress-versions.py
@@ -29,9 +29,9 @@ goal = "unsat"
 goaltotal = 100
 originalfilescopelist = thisdirprefix + "results/2020-09-01-unsat-original-best-scope.txt"
 
-filecounter = 27  # start at first line of files; counting starts at 1
+filecounter = 1  # start at first line of files; counting starts at 1
                  # change above if process does not finish and we have to restart
-goalcounter = 16 # need to reset this if change filecounter!!!
+goalcounter = 0 # need to reset this if change filecounter!!!
 
 now = datetime.now()
 dt_string = now.strftime("%Y-%m-%d-%H-%M-%S")
